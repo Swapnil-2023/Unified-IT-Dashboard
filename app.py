bc1e77dfc8254bed7dca98a9f89e681c3fda6b6c
Swapnil Deorukhkar <swapnilsd170@gmail.com>
2026-04-27 17:25:24 +0530

    Phase-3 completed. Added Asset Managment Module(Real Data)

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from flask import Flask
 from config import Config
 from models import db
 from routes.dashboard import dashboard_bp
+from routes.asset import asset_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.register_blueprint(dashboard_bp)
+app.register_blueprint(asset_bp)
 
 db.init_app(app)
 
---
+++ routes/asset.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, request, jsonify
+from models.asset import Asset
+from models import db
+
+asset_bp = Blueprint('asset', __name__)
+
+# GET all assets
+@asset_bp.route('/assets', methods=['GET'])
+def get_assets():
+    assets = Asset.query.all()
+    return jsonify([
+        {
+            "id": a.id,
+            "name": a.name,
+            "type": a.type,
+            "status": a.status
+        } for a in assets
+    ])
+
+# ADD asset
+@asset_bp.route('/assets', methods=['POST'])
+def add_asset():
+    data = request.json
+
+    asset = Asset(
+        name=data['name'],
+        type=data['type'],
+        status="Active"
+    )
+
+    db.session.add(asset)
+    db.session.commit()
+
+    return jsonify({"message": "Asset added successfully"})
