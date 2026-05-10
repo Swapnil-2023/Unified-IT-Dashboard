80446e875d4219c1d1ab84ec70a1db5b82bf877b
Swapnil Deorukhkar <swapnilsd170@gmail.com>
2026-05-10 15:31:22 +0530

    Added automatic default admin creation

--- init_db.py
+++ init_db.py
@@ -9,5 +9,27 @@ from models.vendor import Vendor
 from models.document import Document
 
 with app.app_context():
+
+    # Create all tables
     db.create_all()
+
+    # Create default admin user
+    admin = User.query.filter_by(
+        email='admin@dashboard.local'
+    ).first()
+
+    if not admin:
+
+        admin = User(
+            name='Admin',
+            email='admin@dashboard.local',
+            password='admin123',
+            role='Admin'
+        )
+
+        db.session.add(admin)
+        db.session.commit()
+
+        print("Default admin user created!")
+
     print("Database created successfully!")
